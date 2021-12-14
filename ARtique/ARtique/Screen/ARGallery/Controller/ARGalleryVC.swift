245b15f4dbe7b21b2e5ff0ecc048feb3cfb2d576
hwangJi-dev <melon2228@swu.ac.kr>
2021-12-14 18:17:33 +0900

    ⚰️[DEL] : 코드 정리

--- ARtique/ARtique/Screen/ARGallery/Controller/ARGalleryVC.swift
+++ ARtique/ARtique/Screen/ARGallery/Controller/ARGalleryVC.swift
@@ -9,73 +9,31 @@ import UIKit
 import ARKit
 import SceneKit
 
-class ARGalleryVC: UIViewController, ARSCNViewDelegate {
+class ARGalleryVC: UIViewController {
 
-    // MARK: - Properties
+    // MARK: Properties
     @IBOutlet var gallerySceneView: ARSCNView!
-    
-    //nodes
     var defaultGalleryNode: SCNNode!
-    var frame1: SCNNode!
-    var frame2: SCNNode!
-    var frame3: SCNNode!
-    var frame4: SCNNode!
-    var frame5: SCNNode!
-    public let camera = SCNNode()
     
+    // MARK: Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBarController?.tabBar.isHidden = true
         setupGallerySceneView()
+        self.tabBarController?.tabBar.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = true
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        
-        // Run the view's session
-        gallerySceneView.session.run(configuration)
+        createSessonConfiguration(gallerySceneView)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         gallerySceneView.session.pause()
     }
     
-    func setupGallerySceneView() {
-        gallerySceneView.delegate = self
-//        gallerySceneView.showsStatistics = true
-        
-        // Create a new scene
-        let galleryScene = SCNScene(named: "art.scnassets/DefaultModel/defaultGallery.scn")!
-        
-        gallerySceneView.scene = galleryScene
-//
-//        camera.camera = SCNCamera()
-//        camera.position = SCNVector3(x: 0, y: -70, z: 50)
-//        camera.eulerAngles = SCNVector3(x: 0.74533, y: 0, z: 0)
-//
-//        frame1 = galleryScene.rootNode.childNode(withName: Identifiers.defaultFrame1, recursively: false)!
-//        frame2 = galleryScene.rootNode.childNode(withName: Identifiers.defaultFrame2, recursively: false)!
-//        frame3 = galleryScene.rootNode.childNode(withName: Identifiers.defaultFrame3, recursively: false)!
-//        frame4 = galleryScene.rootNode.childNode(withName: Identifiers.defaultFrame4, recursively: false)!
-//        frame5 = galleryScene.rootNode.childNode(withName: Identifiers.defaultFrame5, recursively: false)!
-//
-//        let nodes = [frame1, frame2, frame3, frame4, frame5]
-//        for i in 0...nodes.count - 1 {
-//            let location = nodes[i]!.convertPosition(nodes[i]!.position, to: galleryScene.rootNode)
-//            let radius = nodes[i]!.position.y
-//            let moveCameraAc = SCNAction.move(to: location, duration: 2)
-//            moveCameraAc.timingMode = .easeInEaseOut
-//            camera.runAction(moveCameraAc)
-//        }
-    }
-    
+    //MARK: IBAction
     @IBAction func switchOnOffDidTap(_ sender: UISwitch) {
         if !sender.isOn {
             
@@ -85,6 +43,28 @@ class ARGalleryVC: UIViewController, ARSCNViewDelegate {
             gallerySceneView.scene.rootNode.childNodes.filter({ $0.name == Identifiers.defaultGalleryModel }).forEach({ $0.isHidden = false })
         }
     }
+}
+//MARK: - AR Functions
+extension ARGalleryVC: ARSCNViewDelegate {
+    
+    //MARK: Custom Method
+    func createSessonConfiguration(_ arView: ARSCNView) {
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        arView.session.run(configuration)
+    }
+    
+    func setupGallerySceneView() {
+        gallerySceneView.delegate = self
+        
+        // Create a new scene
+        let galleryScene = SCNScene(named: Identifiers.defaultGalleryScenePath)!
+        gallerySceneView.scene = galleryScene
+    }
+    
+    //MARK: AR session
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
