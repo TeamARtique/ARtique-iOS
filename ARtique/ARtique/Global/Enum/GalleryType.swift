4eb6191050e455da4b62b6099c2d8cb9ba98f410
hwangJi-dev <melon2228@swu.ac.kr>
2022-03-19 20:23:01 +0900

    ➕[ADD] : GalleryType 판별을 위한 Enum 생성 (#25)

---
+++ ARtique/ARtique/Global/Enum/GalleryType.swift
@@ -0,0 +1,14 @@
+//
+//  GalleryType.swift
+//  ARtique
+//
+//  Created by hwangJi on 2022/03/19.
+//
+
+import Foundation
+
+enum GalleyType {
+    case minimum
+    case medium
+    case maximum
+}
