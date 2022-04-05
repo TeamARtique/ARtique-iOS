7cd85460ff79f02999d6a4d76cc897c58996ea71
Hwang <dudhkd98@naver.com>
2022-04-06 00:38:22 +0900

    [ADD]: 전시 상세 뷰 naviRightBtn

--- ARtique/ARtique/Screen/Home/Controller/DetailVC.swift
+++ ARtique/ARtique/Screen/Home/Controller/DetailVC.swift
@@ -17,7 +17,9 @@ class DetailVC: UIViewController {
     @IBOutlet weak var likeBtn: UIButton!
     @IBOutlet weak var bookMarkBtn: UIButton!
     @IBOutlet weak var gotoARBtn: UIButton!
+    @IBOutlet weak var naviRightBtn: UIButton!
     
+    var isAuthor: Bool?
     var phosterTmp:UIImage?
     var titleTmp:String?
     var authorTmp:String?
@@ -26,8 +28,11 @@ class DetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // TODO: - 서버연결 후 연결
+        isAuthor = false
         configureView()
         setView()
+        configureMenuBtn()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,15 +60,6 @@ class DetailVC: UIViewController {
         bookMarkBtn.toggleButtonImage(bookMarkBtn.isSelected, UIImage(named: "BookMark_UnSelected")!, UIImage(named: "BookMark_Selected")!)
     }
     
-    @IBAction func didTapShareBtn(_ sender: Any) {
-        // TODO: - Firebase 연동 후 동적 URL 생성
-        let url = "Exhibition URL"
-        let activityVC = UIActivityViewController(activityItems: [url, phoster.image ?? UIImage()], applicationActivities: nil)
-        activityVC.popoverPresentationController?.sourceView = self.view
-        
-        self.present(activityVC, animated: true, completion: nil)
-    }
-    
     @IBAction func goToARGalleryBtnDidTap(_ sender: UIButton) {
         let galleryVC = ViewControllerFactory.viewController(for: .arGallery)
         galleryVC.modalPresentationStyle = .fullScreen
@@ -84,6 +80,31 @@ extension DetailVC {
         likeBtn.isSelected = false
         bookMarkBtn.isSelected = false
     }
+    
+    func configureMenuBtn() {
+        if isAuthor ?? false {
+            naviRightBtn.setImage(UIImage(systemName: "ellipsis"), for: .normal)
+            let share = UIAction(title: "공유",
+                                 image: UIImage(named: "Share")) { _ in
+                self.didTapShareBtn()
+            }
+            let edit = UIAction(title: "수정",
+                                image: UIImage(named: "Edit"),
+                                handler: { _ in print("수정") })
+            let delete = UIAction(title: "삭제",
+                                  image: UIImage(named: "Delete"),
+                                  handler: { _ in print("삭제") })
+            naviRightBtn.menu = UIMenu(title: "",
+                                  image: UIImage(systemName: "heart.fill"),
+                                  identifier: nil,
+                                  options: .displayInline,
+                                  children: [share, edit, delete])
+            naviRightBtn.showsMenuAsPrimaryAction = true
+        } else {
+            naviRightBtn.setImage(UIImage(named: "Share"), for: .normal)
+            naviRightBtn.addTarget(self, action: #selector(didTapShareBtn), for: .touchUpInside)
+        }
+    }
 }
 
 //MARK: - Custom Method
@@ -100,5 +121,14 @@ extension DetailVC {
         gotoARBtn.setTitle("AR 전시 보러가기  →", for: .normal)
         gotoARBtn.titleLabel?.font = UIFont.AppleSDGothicB(size: 16)
         gotoARBtn.layer.cornerRadius = gotoARBtn.frame.height/2
+    }
+    
+    @objc func didTapShareBtn() {
+        // TODO: - Firebase 연동 후 동적 URL 생성
+        let url = "Exhibition URL"
+        let activityVC = UIActivityViewController(activityItems: [url, phoster.image ?? UIImage()], applicationActivities: nil)
+        activityVC.popoverPresentationController?.sourceView = self.view
+        
+        self.present(activityVC, animated: true, completion: nil)
     }
 }
