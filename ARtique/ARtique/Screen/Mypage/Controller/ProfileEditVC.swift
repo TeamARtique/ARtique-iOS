62f242abfe7f2eeef966b9b12f6d056784140801
Hwang <dudhkd98@naver.com>
2022-04-11 18:40:37 +0900

    [FEAT]: 화면 레이아웃 구성

--- ARtique/ARtique/Screen/Mypage/Controller/ProfileEditVC.swift
+++ ARtique/ARtique/Screen/Mypage/Controller/ProfileEditVC.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class ProfileEditVC: UIViewController {
+    @IBOutlet weak var profileImg: UIButton!
+    @IBOutlet weak var nicknameTF: UITextField!
+    @IBOutlet weak var explanationTV: UITextView!
+    @IBOutlet weak var snsTF: UITextField!
+    
+    let textViewMaxCnt = 100
+    var explanationPlaceholder = "ARTI들에게 자신을 소개해보세요!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNaviBar()
+        configureContentView()
     }
 }
 
@@ -27,6 +36,16 @@ extension ProfileEditVC {
                                                   target: self,
                                                   action: #selector(bindRightBarButton))
     }
+    
+    private func configureContentView() {
+        profileImg.layer.cornerRadius = profileImg.frame.height / 2
+        profileImg.layer.masksToBounds = true
+        nicknameTF.setRoundTextField(with: "ARTI")
+        explanationTV.setRoundTextView()
+        explanationTV.setTextViewPlaceholder(explanationPlaceholder)
+        explanationTV.delegate = self
+        snsTF.setRoundTextField(with: "www.instagram.com")
+    }
 }
 
 // MARK: - Custom Methods
@@ -39,5 +58,30 @@ extension ProfileEditVC {
     @objc func bindRightBarButton() {
         // TODO: - post server
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension ProfileEditVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        guard textView.textColor == .textViewPlaceholder else { return }
+        textView.textColor = .label
+        textView.text = ""
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.setTextViewPlaceholder(explanationPlaceholder)
+        }
+        
+        if textView.text.count > textViewMaxCnt {
+            textView.text.removeLast()
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        guard let str = textView.text else { return true }
+        let newLength = str.count + text.count - range.length
+        return newLength <= textViewMaxCnt + 1
     }
 }
