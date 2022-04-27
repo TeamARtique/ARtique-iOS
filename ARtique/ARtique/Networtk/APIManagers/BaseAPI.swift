61c088e97cc8dbc34c9e474a42c62c7e81171aa6
hwangJi-dev <melon2228@swu.ac.kr>
2022-04-28 02:43:36 +0900

    [ADD]: 네트워크 통신 Util 파일 추가 및 BaseAPI Class 생성

--- ARtique/ARtique/Networtk/APIManagers/BaseAPI.swift
+++ ARtique/ARtique/Networtk/APIManagers/BaseAPI.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class BaseAPI {
+    
+    // MARK: Judge
+    func judgeStatus<T: Codable>(type: T.Type, by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodeData = try? decoder.decode(GenericResponse<T>.self, from: data) else {
+            return .pathErr
+        }
+        switch statusCode {
+        case 200: return .success(decodeData.data ?? "None-Data")
+        case 400...409: return .requestErr(decodeData)
+        case 500: return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
