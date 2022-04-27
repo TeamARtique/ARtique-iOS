d5358c3b9052aa18ea2a520f2b6bcb2646ef2771
hwangJi-dev <melon2228@swu.ac.kr>
2022-04-28 02:46:56 +0900

    [CHORE]: 로그인시 유저 정보를 저장할 Singleton 생성, refreshToken을 저장할 Userdefaults key 정의

--- ARtique/ARtique/Global/Class/UserInfo.swift
+++ ARtique/ARtique/Global/Class/UserInfo.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class UserInfo {
+    
+    /// 유저 관리를 위한 싱글톤 객체 선언
+    static let shared = UserInfo()
+    
+    var userID: Int?
+    var userEmail: String?
+    var nickname: String?
+    var accessToken: String?
+    
+    private init() {}
+}
--- ARtique/ARtique/Global/Extension/UserDefaults+.swift
+++ ARtique/ARtique/Global/Extension/UserDefaults+.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 extension UserDefaults {
-    // UserDefaults key value가 많아지면 관리하기 어려워지므로 enum 'Keys'로 묶어 관리
     enum Keys {
         static var loginUserName = "loginUserName"
+        static var refreshToken = "refreshToken"
     }
 }
