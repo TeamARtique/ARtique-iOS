2df6f452572502faa15fa801e1af411180b1398d
hwangJi-dev <melon2228@swu.ac.kr>
2022-04-28 02:40:37 +0900

    [CHORE]: 제네릭타입을 활용한 화면전환 메서드 구현

--- ARtique/ARtique/Global/Enum/NaviType.swift
+++ ARtique/ARtique/Global/Enum/NaviType.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+enum NaviType {
+    case push
+    case present
+}
--- ARtique/ARtique/Global/Extension/String+.swift
+++ ARtique/ARtique/Global/Extension/String+.swift
@@ -5,4 +5,15 @@
 //  Created by hwangJi on 2022/04/28.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    public func getViewController() -> UIViewController? {
+        if let appName = Bundle.main.infoDictionary?["CFBundleName"] as? String {
+            if let viewControllerType = Bundle.main.classNamed("\(appName).\(self)") as? UIViewController.Type {
+                return viewControllerType.init()
+            }
+        }
+        return nil
+    }
+}
