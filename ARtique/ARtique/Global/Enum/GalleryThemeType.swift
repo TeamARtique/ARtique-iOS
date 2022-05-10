228851076a85ab11b815066cf14bb27a2845b24c
hwangJi-dev <melon2228@swu.ac.kr>
2022-05-11 06:38:53 +0900

    [CHORE]: GalleryType, GalleryThemeType Enum 수정

--- ARtique/ARtique/Global/Enum/GalleryThemeType.swift
+++ ARtique/ARtique/Global/Enum/GalleryThemeType.swift
@@ -6,10 +6,67 @@
 //
 
 import Foundation
+import UIKit
 
 enum GalleryThemeType: Int {
     case white = 1
     case dark = 2
     case container = 3
-    case wood = 4
+    case antique = 4
 }
+
+extension GalleryThemeType {
+    
+    /// 갤러리 모델 배경색 or 이미지
+    var galleryModelMaterial: Any {
+        switch self {
+        case .white:
+            return UIColor.white
+        case .dark:
+            return UIColor.darkModel
+        case .container:
+            return UIImage(named: "art.scnassets/GalleryAsset/container.png") ?? UIImage()
+        case .antique:
+            return UIImage(named: "art.scnassets/GalleryAsset/wood.png") ?? UIImage()
+        }
+    }
+    
+   /// 액자 프레임 색
+    var frameColor: UIColor {
+        switch self {
+        case .white:
+            return .black
+        case .dark:
+            return .black
+        case .container:
+            return .white
+        case .antique:
+            return .antiqueFrame
+        }
+    }
+    
+    /// 조명 색
+    var lightColor: UIColor {
+        switch self {
+        case .white, .dark, .container:
+            return .white
+        case .antique:
+            return .antiqueLight
+        }
+    }
+    
+    /// 타이틑 텍스트 색
+    var textColor: UIColor {
+        switch self {
+        case .white:
+            return .defaultText
+        case .dark:
+            return .darkText
+        case .container:
+            return .white
+        case .antique:
+            return .antiqueText
+        }
+    }
+}
+
--- ARtique/ARtique/Global/Enum/GalleryType.swift
+++ ARtique/ARtique/Global/Enum/GalleryType.swift
@@ -12,3 +12,49 @@ enum GalleyType: Int {
     case medium = 12
     case maximum = 30
 }
+
+extension GalleyType {
+    var galleryModelIdentifier: String {
+        switch self {
+        case .minimum:
+            return Identifiers.minimumGalleryModel
+        case .medium:
+            return Identifiers.mediumGalleryModel
+        case .maximum:
+            return Identifiers.maximumGalleryModel
+        }
+    }
+    
+    var galleryScenePath: String {
+        switch self {
+        case .minimum:
+            return Identifiers.minimumGalleryScenePath
+        case .medium:
+            return Identifiers.mediumGalleryScenePath
+        case .maximum:
+            return Identifiers.maximumGalleryScenePath
+        }
+    }
+    
+    /// 액자 프레임 Identifier
+    var frameIdentifier1: String {
+        switch self {
+        case .minimum:
+            return Identifiers.minimumFrame
+        case .medium:
+            return Identifiers.mediumFrame
+        case .maximum:
+            return Identifiers.maximumUpFrame
+        }
+    }
+    
+    /// 액자 프레임 Identifier2 -> maximum model에서 1층 프레임 Identifier를 구분짓기 위해 사용
+    var frameIdentifier2: String {
+        switch self {
+        case .minimum, .medium:
+            return ""
+        case .maximum:
+            return Identifiers.maximumDownFrame
+        }
+    }
+}
