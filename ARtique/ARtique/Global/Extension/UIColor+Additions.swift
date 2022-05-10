a4a4e5c104f31faea123ba77eafb7f8575656f72
hwangJi-dev <melon2228@swu.ac.kr>
2022-05-11 06:38:25 +0900

    [ADD]: 필요한 ColorSet, Identifiers 추가

--- ARtique/ARtique/Global/Extension/UIColor+Additions.swift
+++ ARtique/ARtique/Global/Extension/UIColor+Additions.swift
@@ -30,7 +30,27 @@ extension UIColor {
         return UIColor(red: 53.0 / 255.0, green: 53.0 / 255.0, blue: 53.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var woodFrame: UIColor {
+    @nonobjc class var darkModel: UIColor {
+        return UIColor(red: 53.0 / 255.0, green: 49.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var antiqueFrame: UIColor {
         return UIColor(red: 211.0 / 255.0, green: 168.0 / 255.0, blue: 103.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var antiqueLight: UIColor {
+        return UIColor(red: 225.0 / 255.0, green: 208.0 / 255.0, blue: 117.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var defaultText: UIColor {
+        return UIColor(red: 138.0 / 255.0, green: 138.0 / 255.0, blue: 138.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var darkText: UIColor {
+        return UIColor(red: 233.0 / 255.0, green: 232.0 / 255.0, blue: 235.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var antiqueText: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 231.0 / 255.0, blue: 194.0 / 255.0, alpha: 1.0)
     }
 }
