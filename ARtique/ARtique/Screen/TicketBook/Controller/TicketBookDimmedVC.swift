15b017116d8c05a712eec75f31a7222c4c7e0a00
hwangJi-dev <melon2228@swu.ac.kr>
2022-05-23 12:03:35 +0900

    [ADD]: 필요한 에셋, 파일 추가

---
+++ ARtique/ARtique/Screen/TicketBook/Controller/TicketBookDimmedVC.swift
@@ -0,0 +1,29 @@
+//
+//  TicketBookDimmedVC.swift
+//  ARtique
+//
+//  Created by hwangJi on 2022/05/20.
+//
+
+import UIKit
+
+class TicketBookDimmedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
