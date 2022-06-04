67218ba6c9a70237c5165084f61e78c489f7a0fd
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-04 13:17:09 +0900

    [FEAT]: AR 아트워크를 설명하는 설명뷰 구현

--- ARtique/ARtique/Screen/ARGallery/Controller/ARDescVC.swift
+++ ARtique/ARtique/Screen/ARGallery/Controller/ARDescVC.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import Then
 
-class ARDescVC: UIViewController {
-
+class ARDescVC: BaseVC {
+    
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var betweenView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var seeARBtn: UIButton!
+    
+    var titleText: String?
+    var descText: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
+        setOptionalValues()
+        setTapGuesture()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setOptionalValues() {
+        if titleText != "" && descText == "" {
+            betweenView.isHidden = true
+            descLabel.isHidden = true
+            titleLabel.text = titleText
+        } else if titleText == "" && descText == "" {
+            betweenView.isHidden = true
+            titleLabel.isHidden = true
+            descLabel.text = "등록된 작품 제목과 설명이 없습니다."
+        } else if titleText == "" && descText != "" {
+            betweenView.isHidden = true
+            titleLabel.isHidden = true
+            descLabel.text = descText
+        } else {
+            titleLabel.text = titleText
+            descLabel.text = descText
+        }
+        
+        titleLabel.sizeToFit()
+        descLabel.sizeToFit()
     }
-    */
-
+    
+    
+    private func configureUI() {
+        backView.layer.cornerRadius = 15
+        backView.clipsToBounds = true
+        backView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        
+        seeARBtn.layer.cornerRadius = seeARBtn.frame.height / 2
+        seeARBtn.clipsToBounds = true
+    }
+    
+    private func setTapGuesture() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissVC))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @IBAction func seeARBtnDidTap(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
 }
